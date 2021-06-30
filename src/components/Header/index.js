c49eb39eb7340167d4e5b1add8c4830f7ffbca7d
viictorhoffmann <viictorhoffmann@gmail.com>
2021-06-30 17:48:45 -0300

    feature: adicionando menu hamburguer simples

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,15 +4,22 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import SearchIcon from '@material-ui/icons/Search';
 import './style.css'
+import { useState } from 'react';
 
 const Header = (props) => {
+
+    const [ativo, setAtivo] = useState(false)
+
+    const toggleMenu = () => {
+        setAtivo(!ativo)
+    };
 
     return (
         <header>
             <div className="maxWidth">
                 <nav>
                     <div className="menuHeader">
-                        <MenuIcon />
+                        <button onClick={toggleMenu}><MenuIcon /></button>
                     </div>
                     <div className="logoHeader">
                         <img src={logo} alt="Logo Corebiz" />
@@ -36,6 +43,12 @@ const Header = (props) => {
                     </div>
                 </nav>
             </div>
+            {ativo ? 
+                <div className="userToggle">
+                    <PersonOutlineIcon />
+                    <p>Minha conta</p>
+                </div> :
+                null }
             <div className="searchHeader">
                 <div className="busca">
                     <input type="search" placeholder='O que está procurando?'/>
